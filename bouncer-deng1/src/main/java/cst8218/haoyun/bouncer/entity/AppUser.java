bef30a6e11b1164a31ad0b597b904882485bf002
Haoyun253 <deng0055@algonquinlive.com>
2023-12-02 21:33:37 -0500

    ADD USER

---
+++ bouncer-deng1/src/main/java/cst8218/haoyun/bouncer/entity/AppUser.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cst8218.haoyun.bouncer.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.security.enterprise.identitystore.PasswordHash;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+
+/**
+ *
+ * @author dell
+ */
+@Entity
+public class AppUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "GROUPNAME")
+    private String groupname;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "USERNAME")
+    private String username;
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+    @Column(name = "LASTNAME")
+    private String lastname;
+    @Column(name = "BIRTHDAY")
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+     public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
+    public String getPassword() {
+        return "";
+    }
+
+    public void setPassword(String password) {
+         if (password == null || password.trim().isEmpty()) {
+        return;
+    }
+        Instance<? extends PasswordHash> instance = CDI.current().select(Pbkdf2PasswordHash.class);
+        PasswordHash passwordHash = instance.get();
+        passwordHash.initialize(new HashMap<String,String>()); 
+        String passwordEntry = password; 
+        passwordEntry = passwordHash.generate(passwordEntry.toCharArray());
+        this.password = passwordEntry;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getFirstname() {
+        return firstname;
+    }
+    
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AppUser)) {
+            return false;
+        }
+        AppUser other = (AppUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cst8218.haoyun.bouncer.entity.AppUser[ id=" + id + " ]";
+    }
+    
+}
